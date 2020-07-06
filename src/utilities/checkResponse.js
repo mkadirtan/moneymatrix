ca9c750332aa938bfa9b03b587d95044b46e6e80
mkadirtan <muhammedkadirtan@gmail.com>
2020-07-06 15:41:17 +0300

    Removed console.logs

--- src/utilities/checkResponse.js
+++ src/utilities/checkResponse.js
@@ -4,7 +4,6 @@ const createSignature = require("./createSignature");
 module.exports = function(credentials, response, signatureParameters, fields){
     const signature = response.Signature;
     const calcSignature = createSignature(credentials, response, signatureParameters);
-    console.log(calcSignature)
     if(signature !== calcSignature){ return false }
     return _.isEqual(Object.keys(response), fields);
 }
