342e9a3a3e9b50fdb7d94bee48f70591e4c2fef2
mkadirtan <muhammedkadirtan@gmail.com>
2020-07-23 11:54:23 +0300

    Fixed an error with checking type parameter

--- src/endPoint/index.js
+++ src/endPoint/index.js
@@ -4,13 +4,13 @@ module.exports = function(url){
     const parameters = require("./" + url);
     return {
         createBody(credentials, body, type){
-            if(type !== "request" || type !== "response"){
+            if(type !== "request" && type !== "response"){
                 throw new Error("Invalid type, it must be 'request' or 'response'");
             }
             return util.createBody(credentials, body,  parameters[type].signature);
         },
         checkSignature(credentials, response, type){
-            if(type !== "request" || type !== "response"){
+            if(type !== "request" && type !== "response"){
                 throw new Error("Invalid type, it must be 'request' or 'response'");
             }
             return util.checkSignature(credentials, response, parameters[type].signature);
